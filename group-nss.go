62a087cc51069e5604b17536654a45c77d89f940
Nicolas Surleraux <nsurleraux@gmail.com>
2020-06-04 10:28:21 +0100

    Remove unused C helpers

--- group-nss.go
+++ group-nss.go
@@ -1,24 +1,5 @@
 package nss
 
-//#include <grp.h>
-//#include <errno.h>
-//#include <stdlib.h>
-/*
-static char**makeCharArray(int size) {
-	return calloc(sizeof(char*), size);
-}
-
-static void setArrayString(char **a, char *s, int n) {
-	a[n] = s;
-}
-
-static void freeCharArray(char **a, int size) {
-	int i;
-	for (i = 0; i < size; i++)
-		free(a[i]);
-	free(a);
-}
-*/
 import "C"
 
 import (
